3771015e3a68630d74f4f2f2a1ece0e562e66f3c
Nody Risky Pratomo <nody@Nodys-MacBook-Air.local>
2024-08-02 14:12:07 +0700

    Add method GET, DEL, and PATCH

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4} from 'uuid'
 const router = express.Router();
 
 //Mock database
-const users = [
+let users = [
     {
         first_name: "John",
         last_name: "Doe",
@@ -27,6 +27,37 @@ router.post('/', (req, res) => {
     users.push({ ...user, id: uuidv4() });
 
     res.send(`${user.first_name} has been added to the Database`);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id == id)
+
+    res.send(foundUser)
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id)
+
+    res.send(`${id} is deleted!`);
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const {first_name, last_name, email} = req.body;
+
+    const user = users.find((user) => user.id == id)
+
+    if (first_name) user.first_name = first_name;
+    if (last_name) user.last_name = last_name;
+    if (email) user.email = email;
+
+    res.send(`User with id: ${id} has been updated!`)
+
 });
 
 export default router
